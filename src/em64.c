c79584f7400bf3cda78f86e544482986ded33275
Nick Becker <becker.nick.a@gmail.com>
2015-11-19 20:52:10 -0600

    added includes for coprocessors

--- src/em64.c
+++ src/em64.c
@@ -2,9 +2,14 @@
 //  Nick Becker
 //  19 November, 2015
 
+#include <stdint.h>
+
 #include "cpu.h"
+#include "fpu.h"
 #include "graphics.h"
 #include "memory.h"
+#include "mmu.h"
+#include "rcp.h"
 
 int main(int argc, char **argv) {
   /*
