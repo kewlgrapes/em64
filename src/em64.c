ba3fef8e3d4d5488ea3fd749b5c7d67dff8af9b3
Nick Becker <becker.nick.a@gmail.com>
2015-11-22 11:59:13 -0600

    test read of a ROM

--- src/em64.c
+++ src/em64.c
@@ -3,6 +3,7 @@
 //  19 November, 2015
 
 #include <stdint.h>
+#include <stdio.h>
 
 #include "cpu.h"
 #include "fpu.h"
@@ -14,6 +15,31 @@
 int main(int argc, char **argv) {
   /*
   */
-  puts("test");
+  if (argc > 1)
+    if (argv[1])
+      read_file(argv[1]);
   return 0;
+}
+
+int read_file(char *filename) {
+  char buff[20];
+  FILE *f;
+  printf("opening %s\n", filename);
+  f = fopen(filename, "rb");
+  if (!f)
+    return 0;
+  printf("opened %s\n", filename);
+
+  fread(buff, sizeof(buff), 1, f);
+  int i;
+  for (i=0; i < sizeof(buff); i+=4)
+    printf("%02b:%02x:%02x:%02x\n",
+      buff[i] & 0xff,
+      buff[1+i] & 0xff,
+      buff[2+i] & 0xff,
+      buff[3+i] & 0xff);
+
+  fclose(f);
+  printf("closed %s\n", filename);
+  return sizeof(buff);
 }
