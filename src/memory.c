afe6fda882b62595b9319ce518e0412f7abd8bff
Nick Becker <becker.nick.a@gmail.com>
2015-11-22 20:16:46 -0600

    created mem_read()

--- src/memory.c
+++ src/memory.c
@@ -2,4 +2,44 @@
 //  Nick Becker
 //  19 November, 2015
 
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "locale.h"
 #include "memory.h"
+
+long mem_read(char *file_name) {
+  /*
+  Read an entire ROM file into memory. Returns the number of bytes read.
+  */
+  FILE *fd;
+  long fsize;
+
+  fd = fopen(file_name, "rb");
+  if (fd)
+    printf("opened %s\n", file_name);
+  else {
+    loc_error(ERR_BADOPEN, file_name);
+    return 0;
+  }
+
+  // get length of file and allocate memory
+  fseek(fd, 0, SEEK_END);
+  fsize = ftell(fd);
+
+  // allocate memory and read the entire file
+  mem_rom = (char *)malloc(fsize);
+  if (mem_rom) {
+    fseek(fd, 0, SEEK_SET);
+    if (!fread(mem_rom, fsize, 1, fd)) {
+      
+    }
+  }
+  else
+    printf("failed to allocate memory\n");
+    
+  fclose(fd);
+  printf("closed %s\n", file_name);
+  return fsize;
+}
--- src/memory.h
+++ src/memory.h
@@ -5,4 +5,17 @@
 #ifndef MEMORY_H
 #define MEMORY_H
 
+// masks to determine endianness
+// all valid rom files should start with one of these
+#define MEM_BIG_ENDIAN   0x80371240 // we will convert into this format
+#define MEM_LIT_ENDIAN   0x40123780
+#define MEM_BYTE_SWAPPED 0x37804012
+#define MEM_WORD_SWAPPED 0x12408037
+
+// pointer to the ROM stored in memory
+static char *mem_rom;
+static char mem_rom_size;
+
+long mem_read(char *file_name);
+
 #endif
