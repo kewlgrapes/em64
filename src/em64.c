955e7fe0ee2a97f26c186c0b13be54052fc3fba2
Nick Becker <becker.nick.a@gmail.com>
2015-11-22 20:16:30 -0600

    set program name and call to print usage

--- src/em64.c
+++ src/em64.c
@@ -8,38 +8,23 @@
 #include "cpu.h"
 #include "fpu.h"
 #include "graphics.h"
+#include "locale.h"
 #include "memory.h"
 #include "mmu.h"
 #include "rcp.h"
 
 int main(int argc, char **argv) {
   /*
+  Main entry point for program.
   */
+  // record the program's invocation name
+  if (argc)
+    loc_prog_name = argv[0];
+
   if (argc > 1)
-    if (argv[1])
-      read_file(argv[1]);
+    printf("read %d bytes from %s\n", (int)mem_read(argv[1]), argv[1]);
+  else
+    loc_print_usage();
+
   return 0;
-}
-
-int read_file(char *filename) {
-  char buff[20];
-  FILE *f;
-  printf("opening %s\n", filename);
-  f = fopen(filename, "rb");
-  if (!f)
-    return 0;
-  printf("opened %s\n", filename);
-
-  fread(buff, sizeof(buff), 1, f);
-  int i;
-  for (i=0; i < sizeof(buff); i+=4)
-    printf("%02b:%02x:%02x:%02x\n",
-      buff[i] & 0xff,
-      buff[1+i] & 0xff,
-      buff[2+i] & 0xff,
-      buff[3+i] & 0xff);
-
-  fclose(f);
-  printf("closed %s\n", filename);
-  return sizeof(buff);
 }
