091f1a907ab9f7442584ea2e9914b1784461cc22
Nick Becker <becker.nick.a@gmail.com>
2015-11-22 20:15:56 -0600

    moved rom reading to memory module

--- src/cpu.c
+++ src/cpu.c
@@ -6,3 +6,5 @@
 
 #include "memory.h"
 #include "cpu.h"
+
+
--- src/cpu.h
+++ src/cpu.h
@@ -7,7 +7,20 @@
 #ifndef CPU_H
 #define CPU_H
 
-//registers
-uint64_t cpu_gen_reg[32];
+// masks to determine endianness
+#define CPU_BIG_ENDIAN    0x80371240 // we will convert to this format
+#define CPU_LIT_ENDIAN    0x40123780
+#define CPU_BYTE_SWAPPED  0x37804012
+#define CPU_WORD_SWAPPPED 0x12408037
+
+// special opcodes
+#define CPU_SPECIAL 0x00
+#define CPU_REGIMM  0x01  // BLTZ
+#define CPU_COP0    0x10  // MFC0
+#define CPU_COP1    0x00  // C.F
+
+// registers
+static uint64_t cpu_gen_reg[32];
+static uint64_t cpu_float_reg[32];
 
 #endif
