3b8bec5901677455a28be2d356e03c3b3f4bd292
Nick Becker <becker.nick.a@gmail.com>
2015-11-23 00:07:11 -0600

    added cpu_init() and cpu_fetch()

--- src/cpu.c
+++ src/cpu.c
@@ -7,4 +7,12 @@
 #include "memory.h"
 #include "cpu.h"
 
-
+uint32_t cpu_fetch() {
+  /*
+  Loads the next instruction into the IR and increments the PC. Returns
+  the fetched instruction.
+  */
+  cpu_ir = *(uint32_t *)(mem_rom + cpu_pc);
+  cpu_pc += 4;
+  return cpu_ir;
+}
--- src/cpu.h
+++ src/cpu.h
@@ -7,12 +7,6 @@
 #ifndef CPU_H
 #define CPU_H
 
-// masks to determine endianness
-#define CPU_BIG_ENDIAN    0x80371240 // we will convert to this format
-#define CPU_LIT_ENDIAN    0x40123780
-#define CPU_BYTE_SWAPPED  0x37804012
-#define CPU_WORD_SWAPPPED 0x12408037
-
 // special opcodes
 #define CPU_SPECIAL 0x00
 #define CPU_REGIMM  0x01  // BLTZ
@@ -20,7 +14,20 @@
 #define CPU_COP1    0x00  // C.F
 
 // registers
-static uint64_t cpu_gen_reg[32];
-static uint64_t cpu_float_reg[32];
+uint64_t cpu_gen_reg[32];
+uint64_t cpu_float_reg[32];
+uint32_t cpu_pc;
+uint32_t cpu_ir;
+
+// functions
+
+/*
+void cpu_init();
+
+Initializes the program counter.
+*/
+#define cpu_init() {cpu_pc = 0;}
+
+uint32_t cpu_fetch();
 
 #endif
